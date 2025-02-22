9fad0a5600005f3a76c1b57edc4739e0f2237a6d
TaYWEe <patryk.wenz@gmail.com>
2025-02-22 16:50:28 +0100

    change to relative path

---
+++ build/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const heading = document.getElementById("heading");
+    const button = document.getElementById("btn");
+
+    button.addEventListener("click", () => {
+        heading.innerText = "You Clicked the Button!";
+    });
+});
+
+document.getElementById("btn").addEventListener("click", () => {
+    window.location.href = "/newpage";
+});
