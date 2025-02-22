603f9c13572a167d74a8f3295152b57a713116ad
TaYWEe <patryk.wenz@gmail.com>
2025-02-22 18:08:02 +0100

    blog+

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const heading = document.getElementById("heading");
     const button = document.getElementById("btn");
+    const buttonBlog = document.getElementById("btnBlog");
 
     button.addEventListener("click", () => {
         heading.innerText = "You Clicked the Button!";
@@ -8,7 +9,13 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 document.getElementById("btn").addEventListener("click", () => {
-    //window.location.href = "/public/newpage.html";
-    window.location.href = "https://patrykwenz-4.github.io/nodePortfolio/newpage.html";
+    window.location.href = "/newpage.html";
+    //window.location.href = "https://patrykwenz-4.github.io/nodePortfolio/newpage.html";
+
+});
+
+document.getElementById("btnBlog").addEventListener("click", () => {
+    window.location.href = "/blog.html";
+    //window.location.href = "https://patrykwenz-4.github.io/nodePortfolio/newpage.html";
 
 });
